689984b85cc48a6f9665de5c308abce52591e6e9
Jess Stayton <jessica.stayton@gmail.com>
2022-05-18 23:15:54 -0600

    added user profile page, edited styles for game and user show pages, refactored parts of app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, redirect, request, g, session, flash
 import requests
 from models import db, connect_db, Category, User, Like
-from forms import NewUser, LoginForm
+from forms import NewUser, LoginForm, ReviewForm
 from sqlalchemy.exc import IntegrityError
 
 CURR_USER = "curr_user"
@@ -51,11 +51,42 @@ def get_category_names(games):
     return category_dict
     
 
-def main_request(base_url, endpoint, x):
+def get_videos_for_game(game_id):
+    """Search for most recent 6 videos on a game based on its game ID"""
+    endpoint = f'/game/videos?limit=6&game_id={game_id}&client_id={client_id}'
+    resp = main_request(base_url, endpoint)
+    json = resp.json()
+    videos = json['videos']
+    return videos
+
+
+def fix_video_embed_link(videos):
+    """Receive videos; change URL format to embeded link format to display in HTML; return iterable list of videos"""
+    videos_list = []
+    for video in videos:
+        link = video['url']
+        title = video['title']
+        base_url='https://www.youtube.com/embed/'
+        id = link[link.find("=")+1::]
+        new_url = base_url+id
+        videos_list.append((title, new_url))
+    return videos_list
+
+
+def get_images_for_game(game_id):
+    """Retrieve alternate images for game based on game_id"""
+    endpoint = f'/game/images?limit=10&id={game_id}&client_id={client_id}'
+    resp = main_request(base_url, endpoint)
+    json = resp.json()
+    images = json['images']
+    return images
+
+
+def main_request(base_url, endpoint):
     """function to cut down on repetition; 
     receives a base url and the endpoint for that url, 
     returns resp obj"""
-    resp = requests.get(base_url + endpoint + f'&skip={x}')
+    resp = requests.get(base_url + endpoint)
     return resp
 
 
@@ -72,6 +103,7 @@ def get_count_of_resp(resp):
     json = resp.json()
     return json['count']
 
+
 def get_likes(user):
     if user:
         likes = user.liked_games
@@ -79,15 +111,31 @@ def get_likes(user):
     else:
         return []
 
+
+def add_or_remove_like(game_id, game_ids_list, user):
+    if game_id in game_ids_list:
+        Like.query.filter_by(game_id=game_id, user_id=user.id).delete()
+    else:
+        liked_game = Like(user_id=g.user.id, game_id=game_id)
+        db.session.add(liked_game)
+
+
+def authorized():
+    if not g.user:
+        flash("Access unauthorized.", "danger")
+        return False
+    else:
+        return
+
 ############################################################################################
-# SEARCH ROUTES
+# SEARCH ROUTES for API
 ############################################################################################
 
 @app.route('/games/top_games')
 def show_homepage():
     """Get game data from BGA based on its rank and display in groups of 12"""
 
-    games = parse_resp(main_request(base_url, f'/search/?order_by=rank&limit=12&client_id={client_id}', 0))
+    games = parse_resp(main_request(base_url, f'/search/?order_by=rank&limit=24&client_id={client_id}'))
     
     category_dict = get_category_names(games)
 
@@ -102,7 +150,7 @@ def show_games_in_category(category_name):
 
     category = Category.query.filter_by(name=category_name).first()
     
-    resp = main_request(base_url, f'/search/?categories={category.id}&limit=12&order_by=rank&client_id={client_id}', 0)
+    resp = main_request(base_url, f'/search/?categories={category.id}&limit=24&order_by=rank&client_id={client_id}')
 
     ### working here on maybe paginating search results?
     print(get_count_of_resp(resp))
@@ -120,7 +168,7 @@ def show_games_in_category(category_name):
 def show_games_by_player_count(num):
     """Show top ranked games based on minimum number of players"""
 
-    games = parse_resp(main_request(base_url, f'/search/?min_players={num}&order_by=rank&limit=12&client_id={client_id}', 0))
+    games = parse_resp(main_request(base_url, f'/search/?min_players={num}&order_by=rank&limit=24&client_id={client_id}'))
 
     category_dict = get_category_names(games)
     game_ids_list = get_likes(g.user)
@@ -132,7 +180,7 @@ def show_games_by_player_count(num):
 def show_games_by_player_range(min_num, max_num):
     """Show top ranked games based on min and max player range"""
 
-    games = parse_resp(main_request(base_url, f'/search/?min_players={min_num}&max_players={max_num}&limit=12&order_by=rank&client_id={client_id}', 0))
+    games = parse_resp(main_request(base_url, f'/search/?min_players={min_num}&max_players={max_num}&limit=24&order_by=rank&client_id={client_id}'))
 
     category_dict = get_category_names(games)
     game_ids_list = get_likes(g.user)
@@ -146,9 +194,9 @@ def search_games_by_name():
     query = request.args.get('query')
 
     if query == '':
-        games = parse_resp(main_request(base_url, f'/search?limit=12&client_id={client_id}', 0))
+        games = parse_resp(main_request(base_url, f'/search?limit=24&client_id={client_id}'))
     else:
-        games = parse_resp(main_request(base_url, f'/search/?name={query}&fuzzy_match=true&limit=12&client_id={client_id}', 0))
+        games = parse_resp(main_request(base_url, f'/search/?name={query}&fuzzy_match=true&limit=24&client_id={client_id}'))
     
     category_dict = get_category_names(games)
     game_ids_list = get_likes(g.user)
@@ -163,19 +211,27 @@ def search_games_by_name():
 def home():
     """If not logged in, render login page;
     if logged in, redirect to /top_games"""
+    if not g.user:
+        return redirect('/login')
     return redirect('/games/top_games')
 
 
 @app.route('/games/game/<game_id>')
 def show_game_page(game_id):
     """Show info page for individual game"""
-    
-    games = parse_resp(main_request(base_url, f'/search/?ids={game_id}&client_id={client_id}', 0))
+
+    form=ReviewForm()
+
+    games = parse_resp(main_request(base_url, f'/search/?ids={game_id}&client_id={client_id}'))
     
     category_dict = get_category_names(games)
     game = games[0]
 
-    return render_template('game_page.html', game=game, category_dict=category_dict)
+    videos = fix_video_embed_link(get_videos_for_game(game_id))
+
+    game_ids_list = get_likes(g.user)
+
+    return render_template('game_page.html', game=game, category_dict=category_dict, form=form, videos=videos, game_ids_list=game_ids_list)
 
 
 @app.route('/users/profile/<int:user_id>')
@@ -186,29 +242,25 @@ def show_user_page(user_id):
 
     game_ids = ','.join([str(id) for id in game_ids_list])
 
-    games = parse_resp(main_request(base_url, f'/search/?ids={game_ids}&client_id={client_id}', 0))
+    games = parse_resp(main_request(base_url, f'/search/?ids={game_ids}&client_id={client_id}'))
 
-    return render_template('show_user.html', user=user, games=games)
+    game_ids_list = get_likes(g.user)
+
+    return render_template('show_user.html', user=user, games=games, game_ids_list=game_ids_list)
 
 
 @app.route('/users/like_game/<game_id>', methods=["POST"])
 def like_game(game_id):
     """Logged in user may like or unlike a game"""
-    if not g.user:
-        flash("Access unauthorized.", "danger")
+    if authorized() == False:
         return redirect("/")
     
-    games = parse_resp(main_request(base_url, f'/search/?ids={game_id}&client_id={client_id}', 0))
-    game = games[0]
-    game_id = game['id']
+    games = parse_resp(main_request(base_url, f'/search/?ids={game_id}&client_id={client_id}'))
+    game_id = games[0]['id']
 
     game_ids_list = get_likes(g.user)
 
-    if game_id in game_ids_list:
-        Like.query.filter_by(game_id=game_id, user_id=g.user.id).delete()
-    else:
-        liked_game = Like(user_id=g.user.id, game_id=game_id)
-        db.session.add(liked_game)
+    add_or_remove_like(game_id, game_ids_list, g.user)
 
     db.session.commit()
 
